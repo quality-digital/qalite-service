f0f6dba9e384fe8f6b03d04389b744464ace0f19
sntooosk <juliano.cassimiro@qualitydigital.global>
2026-03-23 10:40:38 -0300

    fix: release 2.0.0

--- src/interfaces/http/routes.ts
+++ src/interfaces/http/routes.ts
@@ -1,16 +1,13 @@
 import { IncomingMessage, ServerResponse } from 'node:http'
 
 import { SendTaskSummaryUseCase } from '../../application/usecases/send-task-summary.js'
-import { ListBrowserstackBuildsUseCase } from '../../application/usecases/list-browserstack-builds.js'
 import { TaskSummaryPayload } from '../../domain/entities/task-summary.js'
-import { BrowserstackCredentials } from '../../domain/entities/browserstack.js'
 import { json } from './http-response.js'
 import { readJsonBody } from './http-request.js'
 import { RouteTable } from './router.js'
 
 interface RouteDependencies {
   sendTaskSummary: SendTaskSummaryUseCase
-  listBrowserstackBuilds: ListBrowserstackBuildsUseCase
 }
 
 const buildSlackSummaryHandler = (
@@ -23,24 +20,8 @@ const buildSlackSummaryHandler = (
   }
 }
 
-const buildListBrowserstackBuildsHandler = (
-  listBrowserstackBuilds: ListBrowserstackBuildsUseCase,
-): ((req: IncomingMessage, res: ServerResponse) => Promise<void>) => {
-  return async (req, res) => {
-    const credentials = await readJsonBody<BrowserstackCredentials>(req)
-    const builds = await listBrowserstackBuilds.execute(credentials)
-    json(res, 200, { builds })
-  }
-}
-
-export const buildRouteTable = ({
-  sendTaskSummary,
-  listBrowserstackBuilds,
-}: RouteDependencies): RouteTable => ({
+export const buildRouteTable = ({ sendTaskSummary }: RouteDependencies): RouteTable => ({
   '/slack/task-summary': {
     POST: buildSlackSummaryHandler(sendTaskSummary),
-  },
-  '/browserstack/builds': {
-    POST: buildListBrowserstackBuildsHandler(listBrowserstackBuilds),
   },
 })

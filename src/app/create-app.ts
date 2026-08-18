6a8702ec63732c0ec3b7e4a588aeeea090244e7d
Juliano Cassimiro Dos Santos <Juliano.cassimiro@qualitydigital.global>
2026-08-18 09:22:10 -0300

    refactor: simplify task summary flow

--- src/app/create-app.ts
+++ src/app/create-app.ts
@@ -1,12 +1,11 @@
 import { SlackWebhookClient } from '../clients/slack-webhook.client.js'
 import type { AppConfig } from '../config/app-config.js'
+import { createTaskSummaryController } from '../controllers/task-summary.controller.js'
 import { composeMiddleware } from '../middlewares/compose.js'
 import { createCorsMiddleware } from '../middlewares/cors.middleware.js'
 import { createErrorMiddleware } from '../middlewares/error.middleware.js'
 import { requestIdMiddleware } from '../middlewares/request-id.middleware.js'
 import { createRouter } from '../routes/router.js'
-import { createRoutes } from '../routes/routes.js'
-import { TaskSummaryService } from '../services/task-summary.service.js'
 import type { HttpHandler } from '../utils/http.js'
 import type { Logger } from '../utils/logger.js'
 
@@ -23,14 +22,16 @@ export const createApp = (
     config.slack.requestTimeoutMs,
     dependencies.fetchImplementation,
   )
-  const taskSummaryService = new TaskSummaryService(slackClient)
-
   return composeMiddleware(
     [
       createErrorMiddleware(dependencies.logger),
       requestIdMiddleware,
       createCorsMiddleware(config.allowedOrigins),
     ],
-    createRouter(createRoutes(taskSummaryService)),
+    createRouter({
+      '/slack/task-summary': {
+        POST: createTaskSummaryController(slackClient),
+      },
+    }),
   )
 }
